deb95b1fbd32e1a4d189ad279083c9460ef53204
Jason Mow <jason.mow@gmail.com>
2012-11-14 18:04:46 -0500

    Adding get geninq features methods

--- hw3.py
+++ hw3.py
@@ -116,6 +116,36 @@ def get_mpqa_features_wordtype(text, mpqa_dict):
                     weak_pos += 1
     return (strong_pos, strong_neg, strong_neutral, weak_pos, weak_neg, weak_neutral)
 
+def get_geninq_features(text, geninq_dict):
+    pos = 0
+    neg = 0
+    for word in word_tokenize(text.lower()):
+        data = geninq_dict[word]
+        if data[0] == 1:
+            pos += 1
+        if data[1] == 1:
+            neg += 1
+    return (pos, neg)
+
+def get_geninq_features_strength(text, geninq_dict):
+    strong_pos = 0
+    strong_neg = 0
+    weak_pos = 0
+    weak_neg = 0  
+    for word in word_tokenize(text.lower()):
+        data = geninq_dict[word]
+        if data[0] == 1:
+            if data[2] == 1:
+                strong_pos += 1
+            if data[3] == 1:
+                weak_pos += 1
+        if data[1] == 1:
+            if data[2] == 1:
+                strong_neg += 1
+            if data[3] == 1:
+                weak_neg += 1
+    return (strong_pos, strong_neg, weak_pos, weak_neg)
+
 def main():
     pass
 
