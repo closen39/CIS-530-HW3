b33edb986f3a114896ed85759a558492ea96e60d
Jason Mow <jason.mow@gmail.com>
2012-11-14 18:07:14 -0500

    Reordering functions

--- hw3.py
+++ hw3.py
@@ -84,26 +84,6 @@ def get_mpqa_features(text, mpqa_dict):
                 neg += 1
     return (pos, neg, neutral)
 
-
-def get_geninq_lexicon(lexicon_path):
-    geninq_dict = dict()
-    f = open(lexicon_path)
-    for line in f:
-        pos, neg, strong, weak = 0, 0, 0, 0
-        x = word_tokenize(line)
-        word = x[0]
-        if 'Pstv' in x:
-            pos = 1
-        if 'Ngtv' in x:
-            neg = 1
-        if 'Strng' in x:
-            strong = 1
-        if 'Weak' in x:
-            weak = 1
-        geninq_dict[word] = [pos, neg, strong, weak]
-    return geninq_dict
-
-
 def get_mpqa_features_wordtype(text, mpqa_dict):
     weak_neg = 0
     weak_neutral = 0
@@ -135,6 +115,24 @@ def get_mpqa_features_wordtype(text, mpqa_dict):
                     weak_neg += 1
                     weak_pos += 1
     return (strong_pos, strong_neg, strong_neutral, weak_pos, weak_neg, weak_neutral)
+
+def get_geninq_lexicon(lexicon_path):
+    geninq_dict = dict()
+    f = open(lexicon_path)
+    for line in f:
+        pos, neg, strong, weak = 0, 0, 0, 0
+        x = word_tokenize(line)
+        word = x[0]
+        if 'Pstv' in x:
+            pos = 1
+        if 'Ngtv' in x:
+            neg = 1
+        if 'Strng' in x:
+            strong = 1
+        if 'Weak' in x:
+            weak = 1
+        geninq_dict[word] = [pos, neg, strong, weak]
+    return geninq_dict
 
 def get_geninq_features(text, geninq_dict):
     pos = 0
