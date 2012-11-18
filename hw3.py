eb2810b8825fb9f650fb6b66ed26c09b2989abff
Jason Mow <jason.mow@gmail.com>
2012-11-18 16:01:21 -0500

    Fixing == bug

--- hw3.py
+++ hw3.py
@@ -270,7 +270,7 @@ def process_corpus(data_dir, features):
 
             out.write(outstring.rstrip() + "\n")
             i += 1
-    elif features = 4:
+    elif features == 4:
          vec = extract_adjectives('/home1/j/jmow/school/cis530/hw3/xmlTrainingOut')
         for priceline, textline in pmap, tmap:
             price = priceline.split("\t")
@@ -292,7 +292,7 @@ def process_corpus(data_dir, features):
                 outstring += idx + ":" + feature + " "
 
             out.write(outstring.rstrip() + "\n")
-    elif features = 5:
+    elif features == 5:
         pass
 
 def main():
